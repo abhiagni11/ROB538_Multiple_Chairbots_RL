60e04dc96acfb5294b3e12be9d20e384ace6f717
abhijeet <abhijeet.agni.1993@gmail.com>
2018-11-25 20:47:06 -0800

    revised plot_results

--- plot_results.py
+++ plot_results.py
@@ -9,8 +9,6 @@ import matplotlib.pyplot as plt
 from collections import namedtuple
 from itertools import count
 
-
-
 def smooth(y, box_pts):
 	box = np.ones(box_pts)/box_pts
 	y_smooth = np.convolve(y, box, mode='same')
@@ -19,33 +17,41 @@ def smooth(y, box_pts):
 if __name__ == "__main__":
 
 	### Load all the numpy arrays
+	IQL_diff_directory = 'IQL_diff/'
+	IQL_global_directory = 'IQL_global/'
+	VDN_directory = 'VDN/'
+
 
 	### For IQL - diff
-	IQL_diff_stat_system_rewards_mean = np.mean(np.array(all_stat_system_rewards), axis=0)
-	IQL_diff_system_rewards_std_dev = np.std(np.array(all_stat_system_rewards), axis=0)
+	IQL_diff_stat_system_rewards_mean = np.loadtxt(IQL_diff_directory+'all_stat_system_rewards_mean', delimiter=',', unpack=True)
+	IQL_diff_system_rewards_std_dev = np.loadtxt(IQL_diff_directory+'all_stat_system_rewards_std_dev', delimiter=',', unpack=True)
 
-	IQL_diff_system_successes_mean = np.mean(np.array(all_stat_system_successes), axis=0)
-	IQL_diff_system_successes_std_dev = np.std(np.array(all_stat_system_successes), axis=0)
+	IQL_diff_system_successes_mean = np.loadtxt(IQL_diff_directory+'all_stat_system_successes_mean', delimiter=',', unpack=True)
+	IQL_diff_system_successes_std_dev = np.loadtxt(IQL_diff_directory+'all_stat_system_successes_std_dev', delimiter=',', unpack=True)
 
-	stat_n_iter = load(stat_n_iter)
+	stat_n_iter_IQL_diff = np.loadtxt(IQL_diff_directory+'stat_n_iter', delimiter=',', unpack=True)
 
 	### For IQL - global
-	IQL_global_stat_system_rewards_mean = np.mean(np.array(all_stat_system_rewards), axis=0)
-	IQL_global_system_rewards_std_dev = np.std(np.array(all_stat_system_rewards), axis=0)
+	IQL_global_stat_system_rewards_mean = np.loadtxt(IQL_global_directory+'all_stat_system_rewards_mean', delimiter=',', unpack=True)
+	IQL_global_system_rewards_std_dev = np.loadtxt(IQL_global_directory+'all_stat_system_rewards_std_dev', delimiter=',', unpack=True)
 
-	IQL_global_system_successes_mean = np.mean(np.array(all_stat_system_successes), axis=0)
-	IQL_global_system_successes_std_dev = np.std(np.array(all_stat_system_successes), axis=0)
+	IQL_global_system_successes_mean = np.loadtxt(IQL_global_directory+'all_stat_system_successes_mean', delimiter=',', unpack=True)
+	IQL_global_system_successes_std_dev = np.loadtxt(IQL_global_directory+'all_stat_system_successes_std_dev', delimiter=',', unpack=True)
+
+	stat_n_iter_IQL_global = np.loadtxt(IQL_global_directory+'stat_n_iter', delimiter=',', unpack=True)
 
 	### For VDN
-	VDN_stat_system_rewards_mean = np.mean(np.array(all_stat_system_rewards), axis=0)
-	VDN_system_rewards_std_dev = np.std(np.array(all_stat_system_rewards), axis=0)
+	VDN_stat_system_rewards_mean = np.loadtxt(VDN_directory+'all_stat_system_rewards_mean', delimiter=',', unpack=True)
+	VDN_system_rewards_std_dev = np.loadtxt(VDN_directory+'all_stat_system_rewards_std_dev', delimiter=',', unpack=True)
 
-	VDN_system_successes_mean = np.mean(np.array(all_stat_system_successes), axis=0)
-	VDN_system_successes_std_dev = np.std(np.array(all_stat_system_successes), axis=0)
+	VDN_system_successes_mean = np.loadtxt(VDN_directory+'all_stat_system_successes_mean', delimiter=',', unpack=True)
+	VDN_system_successes_std_dev = np.loadtxt(VDN_directory+'all_stat_system_successes_std_dev', delimiter=',', unpack=True)
+
+	stat_n_iter_VDN = np.loadtxt(VDN_directory+'stat_n_iter', delimiter=',', unpack=True)
 
 	##################################################################################
 
-	plot_reward_text = "Plotting rewards over all the episodes | For " + '_n_' + " chairbot in 5X5 env"
+	plot_reward_text = "Global VS Diff"
 
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
@@ -56,26 +62,43 @@ if __name__ == "__main__":
 	smoothening_factor = 1
 
 	# Plot IQL for difference rewards
-	plt.plot(stat_n_iter, smooth(IQL_diff_stat_system_rewards_mean,smoothening_factor), 'k', label='IQL_D', color='#CC4F1B')
-	plt.fill_between(stat_n_iter, smooth(IQL_diff_stat_system_rewards_mean-IQL_diff_system_rewards_std_dev,smoothening_factor), smooth(IQL_diff_stat_system_rewards_mean+IQL_diff_system_rewards_std_dev,smoothening_factor),
+	plt.plot(stat_n_iter_IQL_diff, smooth(IQL_diff_stat_system_rewards_mean,smoothening_factor), 'k', label='IQL_D', color='#CC4F1B')
+	plt.fill_between(stat_n_iter_IQL_diff, smooth(IQL_diff_stat_system_rewards_mean-IQL_diff_system_rewards_std_dev,smoothening_factor), smooth(IQL_diff_stat_system_rewards_mean+IQL_diff_system_rewards_std_dev,smoothening_factor),
 		alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
 
 	# Plot IQL for global rewards
-	plt.plot(stat_n_iter, smooth(IQL_global_stat_system_rewards_mean,smoothening_factor), 'k', label='IQL_G', color='#1B2ACC')
-	plt.fill_between(stat_n_iter, smooth(IQL_global_stat_system_rewards_mean-IQL_global_system_rewards_std_dev,smoothening_factor), smooth(IQL_global_stat_system_rewards_mean+IQL_global_system_rewards_std_dev,smoothening_factor),
+	plt.plot(stat_n_iter_IQL_global, smooth(IQL_global_stat_system_rewards_mean,smoothening_factor), 'k', label='IQL_G', color='#1B2ACC')
+	plt.fill_between(stat_n_iter_IQL_global, smooth(IQL_global_stat_system_rewards_mean-IQL_global_system_rewards_std_dev,smoothening_factor), smooth(IQL_global_stat_system_rewards_mean+IQL_global_system_rewards_std_dev,smoothening_factor),
 		alpha=0.5, edgecolor='#1B2ACC', facecolor='#089FFF')
-
-	# Plot VDN
-	plt.plot(stat_n_iter, smooth(VDN_stat_system_rewards_mean,smoothening_factor), 'k', label='VDN', color='#3F7F4C')
-	plt.fill_between(stat_n_iter, smooth(VDN_stat_system_rewards_mean-VDN_system_rewards_std_dev,smoothening_factor), smooth(VDN_stat_system_rewards_mean+VDN_system_rewards_std_dev,smoothening_factor),
-		alpha=0.5, edgecolor='#3F7F4C', facecolor='#089FFF')
 
 	plt.legend()
 	plt.show()
 
+	plot_reward_text = "IQL VS VDN"
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.set_title(plot_reward_text)
+	ax.set_xlabel('Number of episodes')
+	ax.set_ylabel('System reward')
+
+	smoothening_factor = 1
+
+	# Plot IQL for global rewards
+	plt.plot(stat_n_iter_IQL_global, smooth(IQL_global_stat_system_rewards_mean,smoothening_factor), 'k', label='IQL_G', color='#1B2ACC')
+	plt.fill_between(stat_n_iter_IQL_global, smooth(IQL_global_stat_system_rewards_mean-IQL_global_system_rewards_std_dev,smoothening_factor), smooth(IQL_global_stat_system_rewards_mean+IQL_global_system_rewards_std_dev,smoothening_factor),
+		alpha=0.5, edgecolor='#1B2ACC', facecolor='#089FFF')
+
+	# Plot VDN
+	plt.plot(stat_n_iter_VDN, smooth(VDN_stat_system_rewards_mean,smoothening_factor), 'k', label='VDN', color='#3F7F4C')
+	plt.fill_between(stat_n_iter_VDN, smooth(VDN_stat_system_rewards_mean-VDN_system_rewards_std_dev,smoothening_factor), smooth(VDN_stat_system_rewards_mean+VDN_system_rewards_std_dev,smoothening_factor),
+		alpha=0.5, edgecolor='#3F7F4C', facecolor='#7EFF99')
+
+	plt.legend()
+	plt.show()
 	##################################################################################
 	
-	plot_successes_text = "Plotting successes over all the episodes | For " + '_n_' + " chairbot in 5X5 env"
+	plot_successes_text = "Global VS Diff"
 
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
@@ -86,19 +109,37 @@ if __name__ == "__main__":
 	smoothening_factor = 1
 
 	# Plot IQL for difference rewards
-	plt.plot(stat_n_iter, smooth(IQL_diff_system_successes_mean,smoothening_factor), 'm-', label='IQL_D', color='#CC4F1B')
-	plt.fill_between(stat_n_iter, smooth(IQL_diff_system_successes_mean-IQL_diff_system_successes_std_dev,smoothening_factor), smooth(IQL_diff_system_successes_mean+IQL_diff_system_successes_std_dev,smoothening_factor),
+	plt.plot(stat_n_iter_IQL_diff, smooth(IQL_diff_system_successes_mean,smoothening_factor), 'm-', label='IQL_D', color='#CC4F1B')
+	plt.fill_between(stat_n_iter_IQL_diff, smooth(IQL_diff_system_successes_mean-IQL_diff_system_successes_std_dev,smoothening_factor), smooth(IQL_diff_system_successes_mean+IQL_diff_system_successes_std_dev,smoothening_factor),
 		alpha=0.5, edgecolor='#CC4F1B', facecolor='#FF9848')
 
 	# Plot IQL for global rewards
-	plt.plot(stat_n_iter, smooth(IQL_global_system_successes_mean,smoothening_factor), 'm-', label='IQL_G', color='#1B2ACC')
-	plt.fill_between(stat_n_iter, smooth(IQL_global_system_successes_mean-IQL_global_system_successes_std_dev,smoothening_factor), smooth(IQL_global_system_successes_mean+IQL_global_system_successes_std_dev,smoothening_factor),
+	plt.plot(stat_n_iter_IQL_global, smooth(IQL_global_system_successes_mean,smoothening_factor), 'm-', label='IQL_G', color='#1B2ACC')
+	plt.fill_between(stat_n_iter_IQL_global, smooth(IQL_global_system_successes_mean-IQL_global_system_successes_std_dev,smoothening_factor), smooth(IQL_global_system_successes_mean+IQL_global_system_successes_std_dev,smoothening_factor),
+		alpha=0.5, edgecolor='#1B2ACC', facecolor='#089FFF')
+
+	plt.legend()
+	plt.show()
+
+	plot_successes_text = "Global VS Diff"
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.set_title(plot_successes_text)
+	ax.set_xlabel('Number of episodes')
+	ax.set_ylabel('System successes')
+
+	smoothening_factor = 1
+
+	# Plot IQL for global rewards
+	plt.plot(stat_n_iter_IQL_global, smooth(IQL_global_system_successes_mean,smoothening_factor), 'm-', label='IQL_G', color='#1B2ACC')
+	plt.fill_between(stat_n_iter_IQL_global, smooth(IQL_global_system_successes_mean-IQL_global_system_successes_std_dev,smoothening_factor), smooth(IQL_global_system_successes_mean+IQL_global_system_successes_std_dev,smoothening_factor),
 		alpha=0.5, edgecolor='#1B2ACC', facecolor='#089FFF')
 
 	# Plot VDN
-	plt.plot(stat_n_iter, smooth(VDN_system_successes_mean,smoothening_factor), 'm-', label='VDN', color='#3F7F4C')
-	plt.fill_between(stat_n_iter, smooth(VDN_system_successes_mean-VDN_system_successes_std_dev,smoothening_factor), smooth(VDN_system_successes_mean+VDN_system_successes_std_dev,smoothening_factor),
-		alpha=0.5, edgecolor='#3F7F4C', facecolor='#089FFF')
+	plt.plot(stat_n_iter_VDN, smooth(VDN_system_successes_mean,smoothening_factor), 'm-', label='VDN', color='#3F7F4C')
+	plt.fill_between(stat_n_iter_VDN, smooth(VDN_system_successes_mean-VDN_system_successes_std_dev,smoothening_factor), smooth(VDN_system_successes_mean+VDN_system_successes_std_dev,smoothening_factor),
+		alpha=0.5, edgecolor='#3F7F4C', facecolor='#7EFF99')
 
 	plt.legend()
 	plt.show()
